ce3064b32f466811afbb3dbe17ed491776bdff24
Ibrahim Iqbal <ibrahim@ebs.net.pk>
2024-10-23 18:34:04 +0200

    fix: list now contains real players not just their names

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import input
 from game import YatzyStateMachine
 from input import get_num_players
+from player import Player
 
 
 def main():
     num_players = get_num_players()
-    player_list = list()
+    player_list: list[Player] = list()
     for i in range(num_players):
         player_name = input.get_player_name()
-        player_list.append(player_name)
+        player = Player(player_name)
+        player_list.append(player)
 
     yatzy_game = YatzyStateMachine(players=player_list, max_rounds=13)
 
