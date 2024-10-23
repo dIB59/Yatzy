b8ae9edeeed9ef695911952bd05a890ad6da73b7
Ibrahim Iqbal <ibrahim@ebs.net.pk>
2024-10-23 16:32:48 +0200

    update scorecard initialization

--- player.py
+++ player.py
@@ -4,4 +4,19 @@ from dataclasses import dataclass, field
 @dataclass
 class Player:
     name: str
-    scorecard: dict = field(default_factory=dict)
+    # scorecard for the yatzy game
+    scorecard: dict[str, int] = field(default_factory=lambda: {
+        "Aces": None,
+        "Twos": None,
+        "Threes": None,
+        "Fours": None,
+        "Fives": None,
+        "Sixes": None,
+        "Three of a kind": None,
+        "Four of a kind": None,
+        "Full House": None,
+        "Small Straight": None,
+        "Large Straight": None,
+        "Chance": None,
+        "Yatzy": None
+    })
